a5abd38001edcadc099c0a1101041816ef76be40
Henry Mathew <henrymathew122006@gmail.com>
2026-06-17 12:24:27 +0530

    Initial commit

--- js/app.js
+++ js/app.js
@@ -4,5 +4,12 @@ app.controller('ctrl', function($scope) {
     $scope.count=0;
     $scope.isloggedin=false;
     $scope.students = ["Henry","Mathew","jishin"];
-    
+    $scope.currencyamount= 1000;    
+    $scope.today= new Date();
+    $scope.search="";
+});
+app.directive("clg", function() {
+    return { restrict: 'E',
+        template: '<h1>College</h1>'
+    }
 });
